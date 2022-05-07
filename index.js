fcc5732f751655c3800518064fa3abe7c372bd61
jmagayan <janine-aira.magayanes.extern@axelspringer.com>
2022-05-07 16:29:15 +0200

    installed dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express  =  require("express");
 const mongoose = require('mongoose');
 const booksRoute = require('./routes/books');
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000
 
